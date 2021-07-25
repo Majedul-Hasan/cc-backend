b6a809a64513d9ce06b00f041ff376aed784a4f3
Majedul Hasan <77298488+Majedul-Hasan@users.noreply.github.com>
2021-07-25 23:26:27 +0600

    Payment Route assed to the server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const path = require('path');
 // if(process.env.NODE_ENV !== 'production') require('dotenv').config({ path: './.env' });
 if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+/** stripe librery  */
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
+/** stripe librery  */
+
+
+
+
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -36,5 +45,17 @@ app.listen(port, error=>{
 
 
 app.post('/payment', (req, res)=>{
-   
+   const body = {
+      source: req.body.token.id,
+      amount: req.body.amount,
+      currency: "usd"
+   };
+
+   stripe.charges.create(body, (stripeErr, StripeRes)=>{
+      if(stripeErr){
+         res.status(500).send({error: stripeErr})
+      }else{
+         res.status(200).send({success: StripeRes})
+      }
+   })
 })
